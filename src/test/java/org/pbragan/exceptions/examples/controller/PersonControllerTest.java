86baec4de666dfa67fbba14eca433f1eee6ce811
pablobragan@github.com <pbragan@navent.com>
2022-06-13 11:51:52 -0300

    Se agrega inicializacion de test folder

---
+++ src/test/java/org/pbragan/exceptions/examples/controller/PersonControllerTest.java
@@ -0,0 +1,22 @@
+package org.pbragan.exceptions.examples.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void takeMorningShower() {
+    }
+}
